cebea6f721d40c12c78ad32873ddc9766cb00733
Michael Smith <michaelsmith101@gmail.com>
2017-09-03 02:15:24 +0000

    Completed checkpoint-3 assignment

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,20 @@
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
+   
+   describe "#remove_entry" do
+     it "removes only one entry to the address book" do
+       book = AddressBook.new
+       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       book.add_entry('Michael Smith', '425.867.5309', 'myemail@me.com')
+
+       expect(book.entries.size).to eq 2
+       book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       #expect(book.entries.size).to eq 1
+       #expect(book.entries.first.name).to eq("Michael Smith")
+       
+     end
+ 
+   end
+   
  end
